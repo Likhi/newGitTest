eda4bb3575da6387f2b10c965cac9f4999c93c12
Likhi <likhi.ondov@gmail.com>
2014-04-02 21:48:02 -0700

    Made extremely rude greeting

--- intro.py
+++ intro.py
@@ -12,7 +12,7 @@ def main():
    else:
     name=sys.argv[1]
     #If argument IS added to end of module call
-   print('Why, hello there, ', name, '.', sep='')
+   print('Fuck you, ', name, '.', sep='')
    
    # Command line args are in sys.argv[1], sys.argv[2] ...
    # sys.argv[0] is the script name itself and can be ignored
